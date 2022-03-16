7fec9e5c77230d7288091b94f83e8d255c9071cd
bodeolukolu <bolukolu@utk.edu>
2022-03-16 13:40:35 -0400

    updated link for auto-install of NCBI BLAST

--- scripts/Qmatey_profiling.sh
+++ scripts/Qmatey_profiling.sh
@@ -30,7 +30,7 @@ if [[ "$blast_location" == "custom" ]]; then
 	fi
 	cd $projdir
 	echo -e "$1 \e[31m Creating custom database"
-	${Qmatey_dir}/tools/ncbi-blast-2.12.0+/bin/makeblastdb -in $input_dbfasta -parse_seqids -blastdb_version 10 -taxid_map $map_taxids -title "custom_db" -dbtype nt
+	${Qmatey_dir}/tools/ncbi-blast-2.13.0+/bin/makeblastdb -in $input_dbfasta -parse_seqids -blastdb_version 10 -taxid_map $map_taxids -title "custom_db" -dbtype nt
 fi
 
 
@@ -701,7 +701,7 @@ if [[ "$blast_location" == "local" ]]; then
 			awk 'NR%2000==1{x="subfile"++i;}{print > x}' $ccf & PIDsplit2=$!
 			wait $PIDsplit2
 			for sub in $(ls subfile* | sort -V); do (
-				${Qmatey_dir}/tools/ncbi-blast-2.12.0+/bin/blastn -task megablast -query $sub -db "${local_db}" -num_threads 1 -perc_identity 90 -max_target_seqs 1000000 \
+				${Qmatey_dir}/tools/ncbi-blast-2.13.0+/bin/blastn -task megablast -query $sub -db "${local_db}" -num_threads 1 -perc_identity 90 -max_target_seqs 1000000 \
 				-outfmt "6 qseqid sseqid length qstart qcovs pident qseq sseq staxids stitle" -out ${sub}_out.blast &&
 				gzip ${sub}_out.blast )&
 				if [[ $(jobs -r -p | wc -l) -ge $threads ]]; then
@@ -758,7 +758,7 @@ if [[ "$blast_location" == "remote" ]]; then
 		echo -e "${YELLOW}- BLAST ouput already exist"
 		echo -e "${YELLOW}- Skipping BLAST and filtering hits based on defined parameters"
 	else
-		${Qmatey_dir}/tools/ncbi-blast-2.12.0+/bin/blastn -task megablast -query combined_compressed_metagenomes.fasta -db "${remote_db}" -perc_identity 90  -max_target_seqs 1000000 \
+		${Qmatey_dir}/tools/ncbi-blast-2.13.0+/bin/blastn -task megablast -query combined_compressed_metagenomes.fasta -db "${remote_db}" -perc_identity 90  -max_target_seqs 1000000 \
 		-outfmt "6 qseqid sseqid length qstart qcovs pident gapopen qseq sseq staxids stitle" \
 		-out ../alignment/combined_compressed.megablast -remote
 	fi
@@ -816,7 +816,7 @@ if [[ "$blast_location" == "custom" ]]; then
 			awk 'NR%2000==1{x="subfile"++i;}{print > x}' $ccf & PIDsplit2=$!
 			wait $PIDsplit2
 			for sub in $(ls subfile* | sort -V); do (
-				${Qmatey_dir}/tools/ncbi-blast-2.12.0+/bin/blastn -task megablast -query $sub -db "${custom_db}" -num_threads 1 -perc_identity 90  -max_target_seqs 1000000 \
+				${Qmatey_dir}/tools/ncbi-blast-2.13.0+/bin/blastn -task megablast -query $sub -db "${custom_db}" -num_threads 1 -perc_identity 90  -max_target_seqs 1000000 \
 				-outfmt "6 qseqid sseqid length qstart qcovs pident gapopen qseq sseq staxids stitle" -out ${sub}_out.blast &&
 				wait
 				gzip ${sub}_out.blast )&
