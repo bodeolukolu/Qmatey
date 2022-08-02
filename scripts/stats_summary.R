b866673d0de7aafda741c7d627b673aa687d15fa
bodeolukolu <bolukolu@utk.edu>
2022-08-02 17:17:41 -0400

    abundance: single-copy seq

--- scripts/stats_summary.R
+++ scripts/stats_summary.R
@@ -30,23 +30,34 @@ if (args[2] == "strain"){
     stats1 <- subset(stats1, grepl('^\\d+$', stats1$staxids))
   }
   if (nrow(stats1) > 0) {
+    stats1 <- aggregate(stats1$abundance, by=list(sseqid=stats1$sseqid,stitle=stats1$stitle,staxids=stats1$staxids), FUN=sum)
+    colnames(stats1)[4] <- c("abundance")
     stats1$stitle <- gsub(".*(ribosomal_RNA)", "\\1", stats1$stitle)
-    stats1$stitle <- gsub(".*(rRNA)", "\\1", stats1$stitle)
     stats1$stitle <- gsub("(ribosomal_RNA).*", "\\1", stats1$stitle)
+    stats1$stitle <- gsub(".*(rRNA)", "\\1", stats1$stitle)
     stats1$stitle <- gsub("(rRNA).*", "\\1", stats1$stitle)
+    stats1$abundance2 <- stats1$abundance
     stats1$abundance[stats1$stitle == "ribosomal_RNA"] <- NA
     stats1$abundance[stats1$stitle == "rRNA"] <- NA
-    stats1$abundance2 <- stats1$abundance
-    stats1 <- subset(stats1, select=c(3,1,5))
+    stats1 <- subset(stats1, select=c(3,4,5))
+    stats1$uniqseq <- 0
+    stats1$uniqseq[stats1$abundance >= 0] <- 1
     stats1$abundance <- ifelse(is.na(stats1$abundance), stats1$abundance2, stats1$abundance)
     stats1 <- subset(stats1, select=-c(abundance2))
+    stats2 <- ddply(stats1, c("staxids","uniqseq"), summarise, Nt = length(staxids))
+    stats3 <- as.data.frame(table(stats2$staxids)); colnames(stats3)[1] <- c("staxids")
+    stats1 <- merge(stats1, stats3, by=c("staxids"), all.x=TRUE)
+    stats1$abundance[stats1$uniqseq == "0" & stats1$Freq == "2" ] <- NA
     stats1 <- ddply(stats1, c("staxids"), summarise,
                     N    = length(staxids),
-                    mean = mean(abundance),
-                    sd   = sd(abundance),
-                    se   = sd / sqrt(N)
+                    mean = mean(abundance, na.rm=TRUE),
+                    sd   = sd(abundance, na.rm=TRUE)
     )
-    stats1 <- subset(stats1, select=c(1,3,2,5))
+    stats2 <- merge(stats2, stats3, by=c("staxids"), all.x=TRUE)
+    stats2$Nt[stats2$uniqseq == "0" & stats2$Freq == "2" ] <- NA; stats2 <- na.omit(stats2)
+    stats1 <- merge(stats1, stats2, by=c("staxids"), all.x=TRUE)
+    stats1$se <- stats1$sd/stats1$Nt    
+    stats1 <- subset(stats1, select=c(1,3,2,8))
     colnames(stats1) <- c("taxid","mean","uniq_reads","stderr")
     stats1$rel_stderr <- (stats1$stderr/stats1$mean)*100
   }
@@ -68,26 +79,37 @@ if (args[2] == "species"){
     stats1 <- stats1[!(stats1$sseqid == ""), ]
     stats1 <- stats1[!(stats1$species == ""), ]
     stats1 <- subset(stats1, !grepl('^\\d+$', stats1$sseqid))
-    stats1 <- subset(stats1, !grepl('^\\d+$', stats1$species))
+    stats1 <- subset(stats1, grepl('^\\d+$', stats1$species))
   }
   if (nrow(stats1) > 0) {
+    stats1 <- aggregate(stats1$abundance, by=list(sseqid=stats1$sseqid,stitle=stats1$stitle,species=stats1$species), FUN=sum)
+    colnames(stats1)[4] <- c("abundance")
     stats1$stitle <- gsub(".*(ribosomal_RNA)", "\\1", stats1$stitle)
-    stats1$stitle <- gsub(".*(rRNA)", "\\1", stats1$stitle)
     stats1$stitle <- gsub("(ribosomal_RNA).*", "\\1", stats1$stitle)
+    stats1$stitle <- gsub(".*(rRNA)", "\\1", stats1$stitle)
     stats1$stitle <- gsub("(rRNA).*", "\\1", stats1$stitle)
+    stats1$abundance2 <- stats1$abundance
     stats1$abundance[stats1$stitle == "ribosomal_RNA"] <- NA
     stats1$abundance[stats1$stitle == "rRNA"] <- NA
-    stats1$abundance2 <- stats1$abundance
-    stats1 <- subset(stats1, select=c(3,1,5))
+    stats1 <- subset(stats1, select=c(3,4,5))
+    stats1$uniqseq <- 0
+    stats1$uniqseq[stats1$abundance >= 0] <- 1
     stats1$abundance <- ifelse(is.na(stats1$abundance), stats1$abundance2, stats1$abundance)
     stats1 <- subset(stats1, select=-c(abundance2))
+    stats2 <- ddply(stats1, c("species","uniqseq"), summarise, Nt = length(species))
+    stats3 <- as.data.frame(table(stats2$species)); colnames(stats3)[1] <- c("species")
+    stats1 <- merge(stats1, stats3, by=c("species"), all.x=TRUE)
+    stats1$abundance[stats1$uniqseq == "0" & stats1$Freq == "2" ] <- NA
     stats1 <- ddply(stats1, c("species"), summarise,
                     N    = length(species),
-                    mean = mean(abundance),
-                    sd   = sd(abundance),
-                    se   = sd / sqrt(N)
+                    mean = mean(abundance, na.rm=TRUE),
+                    sd   = sd(abundance, na.rm=TRUE)
     )
-    stats1 <- subset(stats1, select=c(1,3,2,5))
+    stats2 <- merge(stats2, stats3, by=c("species"), all.x=TRUE)
+    stats2$Nt[stats2$uniqseq == "0" & stats2$Freq == "2" ] <- NA; stats2 <- na.omit(stats2)
+    stats1 <- merge(stats1, stats2, by=c("species"), all.x=TRUE)
+    stats1$se <- stats1$sd/stats1$Nt    
+    stats1 <- subset(stats1, select=c(1,3,2,8))
     colnames(stats1) <- c("taxid","mean","uniq_reads","stderr")
     stats1$rel_stderr <- (stats1$stderr/stats1$mean)*100
   }
@@ -99,7 +121,7 @@ if (args[2] == "genus"){
   fileName <- c(sighits)
   stats1 <- read.delim(file=fileName, header=T, sep="\t", fill= T, quote="", check.names = T)
   if (nrow(stats1) > 0) {
-    stats1 <- subset(stats1, select=c(1,2,9))
+    stats1 <- subset(stats1, select=c(1,2,13,9))
     stats1 <- subset(stats1, grepl("^[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?$", stats1$abundance))
     stats1$abundance <- as.numeric(as.character(stats1$abundance))
     stats1 <- stats1[!is.na(stats1$abundance),]
@@ -109,26 +131,37 @@ if (args[2] == "genus"){
     stats1 <- stats1[!(stats1$sseqid == ""), ]
     stats1 <- stats1[!(stats1$genus == ""), ]
     stats1 <- subset(stats1, !grepl('^\\d+$', stats1$sseqid))
-    stats1 <- subset(stats1, !grepl('^\\d+$', stats1$genus))
+    stats1 <- subset(stats1, grepl('^\\d+$', stats1$genus))
   }
   if (nrow(stats1) > 0) {
+    stats1 <- aggregate(stats1$abundance, by=list(sseqid=stats1$sseqid,stitle=stats1$stitle,genus=stats1$genus), FUN=sum)
+    colnames(stats1)[4] <- c("abundance")
     stats1$stitle <- gsub(".*(ribosomal_RNA)", "\\1", stats1$stitle)
-    stats1$stitle <- gsub(".*(rRNA)", "\\1", stats1$stitle)
     stats1$stitle <- gsub("(ribosomal_RNA).*", "\\1", stats1$stitle)
+    stats1$stitle <- gsub(".*(rRNA)", "\\1", stats1$stitle)
     stats1$stitle <- gsub("(rRNA).*", "\\1", stats1$stitle)
+    stats1$abundance2 <- stats1$abundance
     stats1$abundance[stats1$stitle == "ribosomal_RNA"] <- NA
     stats1$abundance[stats1$stitle == "rRNA"] <- NA
-    stats1$abundance2 <- stats1$abundance
-    stats1 <- subset(stats1, select=c(3,1,5))
+    stats1 <- subset(stats1, select=c(3,4,5))
+    stats1$uniqseq <- 0
+    stats1$uniqseq[stats1$abundance >= 0] <- 1
     stats1$abundance <- ifelse(is.na(stats1$abundance), stats1$abundance2, stats1$abundance)
     stats1 <- subset(stats1, select=-c(abundance2))
+    stats2 <- ddply(stats1, c("genus","uniqseq"), summarise, Nt = length(genus))
+    stats3 <- as.data.frame(table(stats2$genus)); colnames(stats3)[1] <- c("genus")
+    stats1 <- merge(stats1, stats3, by=c("genus"), all.x=TRUE)
+    stats1$abundance[stats1$uniqseq == "0" & stats1$Freq == "2" ] <- NA
     stats1 <- ddply(stats1, c("genus"), summarise,
                     N    = length(genus),
-                    mean = mean(abundance),
-                    sd   = sd(abundance),
-                    se   = sd / sqrt(N)
+                    mean = mean(abundance, na.rm=TRUE),
+                    sd   = sd(abundance, na.rm=TRUE)
     )
-    stats1 <- subset(stats1, select=c(1,3,2,5))
+    stats2 <- merge(stats2, stats3, by=c("genus"), all.x=TRUE)
+    stats2$Nt[stats2$uniqseq == "0" & stats2$Freq == "2" ] <- NA; stats2 <- na.omit(stats2)
+    stats1 <- merge(stats1, stats2, by=c("genus"), all.x=TRUE)
+    stats1$se <- stats1$sd/stats1$Nt    
+    stats1 <- subset(stats1, select=c(1,3,2,8))
     colnames(stats1) <- c("taxid","mean","uniq_reads","stderr")
     stats1$rel_stderr <- (stats1$stderr/stats1$mean)*100
   }
@@ -137,7 +170,7 @@ if (args[2] == "genus"){
 }
 
 if (args[2] == "family"){
-fileName <- c(sighits)
+  fileName <- c(sighits)
   stats1 <- read.delim(file=fileName, header=T, sep="\t", fill= T, quote="", check.names = T)
   if (nrow(stats1) > 0) {
     stats1 <- subset(stats1, select=c(1,2,13,9))
@@ -150,26 +183,37 @@ fileName <- c(sighits)
     stats1 <- stats1[!(stats1$sseqid == ""), ]
     stats1 <- stats1[!(stats1$family == ""), ]
     stats1 <- subset(stats1, !grepl('^\\d+$', stats1$sseqid))
-    stats1 <- subset(stats1, !grepl('^\\d+$', stats1$family))
+    stats1 <- subset(stats1, grepl('^\\d+$', stats1$family))
   }
   if (nrow(stats1) > 0) {
+    stats1 <- aggregate(stats1$abundance, by=list(sseqid=stats1$sseqid,stitle=stats1$stitle,family=stats1$family), FUN=sum)
+    colnames(stats1)[4] <- c("abundance")
     stats1$stitle <- gsub(".*(ribosomal_RNA)", "\\1", stats1$stitle)
-    stats1$stitle <- gsub(".*(rRNA)", "\\1", stats1$stitle)
     stats1$stitle <- gsub("(ribosomal_RNA).*", "\\1", stats1$stitle)
+    stats1$stitle <- gsub(".*(rRNA)", "\\1", stats1$stitle)
     stats1$stitle <- gsub("(rRNA).*", "\\1", stats1$stitle)
+    stats1$abundance2 <- stats1$abundance
     stats1$abundance[stats1$stitle == "ribosomal_RNA"] <- NA
     stats1$abundance[stats1$stitle == "rRNA"] <- NA
-    stats1$abundance2 <- stats1$abundance
-    stats1 <- subset(stats1, select=c(3,1,5))
+    stats1 <- subset(stats1, select=c(3,4,5))
+    stats1$uniqseq <- 0
+    stats1$uniqseq[stats1$abundance >= 0] <- 1
     stats1$abundance <- ifelse(is.na(stats1$abundance), stats1$abundance2, stats1$abundance)
     stats1 <- subset(stats1, select=-c(abundance2))
+    stats2 <- ddply(stats1, c("family","uniqseq"), summarise, Nt = length(family))
+    stats3 <- as.data.frame(table(stats2$family)); colnames(stats3)[1] <- c("family")
+    stats1 <- merge(stats1, stats3, by=c("family"), all.x=TRUE)
+    stats1$abundance[stats1$uniqseq == "0" & stats1$Freq == "2" ] <- NA
     stats1 <- ddply(stats1, c("family"), summarise,
                     N    = length(family),
-                    mean = mean(abundance),
-                    sd   = sd(abundance),
-                    se   = sd / sqrt(N)
+                    mean = mean(abundance, na.rm=TRUE),
+                    sd   = sd(abundance, na.rm=TRUE)
     )
-    stats1 <- subset(stats1, select=c(1,3,2,5))
+    stats2 <- merge(stats2, stats3, by=c("family"), all.x=TRUE)
+    stats2$Nt[stats2$uniqseq == "0" & stats2$Freq == "2" ] <- NA; stats2 <- na.omit(stats2)
+    stats1 <- merge(stats1, stats2, by=c("family"), all.x=TRUE)
+    stats1$se <- stats1$sd/stats1$Nt    
+    stats1 <- subset(stats1, select=c(1,3,2,8))
     colnames(stats1) <- c("taxid","mean","uniq_reads","stderr")
     stats1$rel_stderr <- (stats1$stderr/stats1$mean)*100
   }
@@ -191,26 +235,37 @@ if (args[2] == "order"){
     stats1 <- stats1[!(stats1$sseqid == ""), ]
     stats1 <- stats1[!(stats1$order == ""), ]
     stats1 <- subset(stats1, !grepl('^\\d+$', stats1$sseqid))
-    stats1 <- subset(stats1, !grepl('^\\d+$', stats1$order))
+    stats1 <- subset(stats1, grepl('^\\d+$', stats1$order))
   }
   if (nrow(stats1) > 0) {
+    stats1 <- aggregate(stats1$abundance, by=list(sseqid=stats1$sseqid,stitle=stats1$stitle,order=stats1$order), FUN=sum)
+    colnames(stats1)[4] <- c("abundance")
     stats1$stitle <- gsub(".*(ribosomal_RNA)", "\\1", stats1$stitle)
-    stats1$stitle <- gsub(".*(rRNA)", "\\1", stats1$stitle)
     stats1$stitle <- gsub("(ribosomal_RNA).*", "\\1", stats1$stitle)
+    stats1$stitle <- gsub(".*(rRNA)", "\\1", stats1$stitle)
     stats1$stitle <- gsub("(rRNA).*", "\\1", stats1$stitle)
+    stats1$abundance2 <- stats1$abundance
     stats1$abundance[stats1$stitle == "ribosomal_RNA"] <- NA
     stats1$abundance[stats1$stitle == "rRNA"] <- NA
-    stats1$abundance2 <- stats1$abundance
-    stats1 <- subset(stats1, select=c(3,1,5))
+    stats1 <- subset(stats1, select=c(3,4,5))
+    stats1$uniqseq <- 0
+    stats1$uniqseq[stats1$abundance >= 0] <- 1
     stats1$abundance <- ifelse(is.na(stats1$abundance), stats1$abundance2, stats1$abundance)
     stats1 <- subset(stats1, select=-c(abundance2))
+    stats2 <- ddply(stats1, c("order","uniqseq"), summarise, Nt = length(order))
+    stats3 <- as.data.frame(table(stats2$order)); colnames(stats3)[1] <- c("order")
+    stats1 <- merge(stats1, stats3, by=c("order"), all.x=TRUE)
+    stats1$abundance[stats1$uniqseq == "0" & stats1$Freq == "2" ] <- NA
     stats1 <- ddply(stats1, c("order"), summarise,
                     N    = length(order),
-                    mean = mean(abundance),
-                    sd   = sd(abundance),
-                    se   = sd / sqrt(N)
+                    mean = mean(abundance, na.rm=TRUE),
+                    sd   = sd(abundance, na.rm=TRUE)
     )
-    stats1 <- subset(stats1, select=c(1,3,2,5))
+    stats2 <- merge(stats2, stats3, by=c("order"), all.x=TRUE)
+    stats2$Nt[stats2$uniqseq == "0" & stats2$Freq == "2" ] <- NA; stats2 <- na.omit(stats2)
+    stats1 <- merge(stats1, stats2, by=c("order"), all.x=TRUE)
+    stats1$se <- stats1$sd/stats1$Nt    
+    stats1 <- subset(stats1, select=c(1,3,2,8))
     colnames(stats1) <- c("taxid","mean","uniq_reads","stderr")
     stats1$rel_stderr <- (stats1$stderr/stats1$mean)*100
   }
@@ -232,26 +287,37 @@ if (args[2] == "class"){
     stats1 <- stats1[!(stats1$sseqid == ""), ]
     stats1 <- stats1[!(stats1$class == ""), ]
     stats1 <- subset(stats1, !grepl('^\\d+$', stats1$sseqid))
-    stats1 <- subset(stats1, !grepl('^\\d+$', stats1$class))
+    stats1 <- subset(stats1, grepl('^\\d+$', stats1$class))
   }
   if (nrow(stats1) > 0) {
+    stats1 <- aggregate(stats1$abundance, by=list(sseqid=stats1$sseqid,stitle=stats1$stitle,class=stats1$class), FUN=sum)
+    colnames(stats1)[4] <- c("abundance")
     stats1$stitle <- gsub(".*(ribosomal_RNA)", "\\1", stats1$stitle)
-    stats1$stitle <- gsub(".*(rRNA)", "\\1", stats1$stitle)
     stats1$stitle <- gsub("(ribosomal_RNA).*", "\\1", stats1$stitle)
+    stats1$stitle <- gsub(".*(rRNA)", "\\1", stats1$stitle)
     stats1$stitle <- gsub("(rRNA).*", "\\1", stats1$stitle)
+    stats1$abundance2 <- stats1$abundance
     stats1$abundance[stats1$stitle == "ribosomal_RNA"] <- NA
     stats1$abundance[stats1$stitle == "rRNA"] <- NA
-    stats1$abundance2 <- stats1$abundance
-    stats1 <- subset(stats1, select=c(3,1,5))
+    stats1 <- subset(stats1, select=c(3,4,5))
+    stats1$uniqseq <- 0
+    stats1$uniqseq[stats1$abundance >= 0] <- 1
     stats1$abundance <- ifelse(is.na(stats1$abundance), stats1$abundance2, stats1$abundance)
     stats1 <- subset(stats1, select=-c(abundance2))
+    stats2 <- ddply(stats1, c("class","uniqseq"), summarise, Nt = length(class))
+    stats3 <- as.data.frame(table(stats2$class)); colnames(stats3)[1] <- c("class")
+    stats1 <- merge(stats1, stats3, by=c("class"), all.x=TRUE)
+    stats1$abundance[stats1$uniqseq == "0" & stats1$Freq == "2" ] <- NA
     stats1 <- ddply(stats1, c("class"), summarise,
                     N    = length(class),
-                    mean = mean(abundance),
-                    sd   = sd(abundance),
-                    se   = sd / sqrt(N)
+                    mean = mean(abundance, na.rm=TRUE),
+                    sd   = sd(abundance, na.rm=TRUE)
     )
-    stats1 <- subset(stats1, select=c(1,3,2,5))
+    stats2 <- merge(stats2, stats3, by=c("class"), all.x=TRUE)
+    stats2$Nt[stats2$uniqseq == "0" & stats2$Freq == "2" ] <- NA; stats2 <- na.omit(stats2)
+    stats1 <- merge(stats1, stats2, by=c("class"), all.x=TRUE)
+    stats1$se <- stats1$sd/stats1$Nt    
+    stats1 <- subset(stats1, select=c(1,3,2,8))
     colnames(stats1) <- c("taxid","mean","uniq_reads","stderr")
     stats1$rel_stderr <- (stats1$stderr/stats1$mean)*100
   }
@@ -273,29 +339,41 @@ if (args[2] == "phylum"){
     stats1 <- stats1[!(stats1$sseqid == ""), ]
     stats1 <- stats1[!(stats1$phylum == ""), ]
     stats1 <- subset(stats1, !grepl('^\\d+$', stats1$sseqid))
-    stats1 <- subset(stats1, !grepl('^\\d+$', stats1$phylum))
+    stats1 <- subset(stats1, grepl('^\\d+$', stats1$phylum))
   }
   if (nrow(stats1) > 0) {
+    stats1 <- aggregate(stats1$abundance, by=list(sseqid=stats1$sseqid,stitle=stats1$stitle,phylum=stats1$phylum), FUN=sum)
+    colnames(stats1)[4] <- c("abundance")
     stats1$stitle <- gsub(".*(ribosomal_RNA)", "\\1", stats1$stitle)
-    stats1$stitle <- gsub(".*(rRNA)", "\\1", stats1$stitle)
     stats1$stitle <- gsub("(ribosomal_RNA).*", "\\1", stats1$stitle)
+    stats1$stitle <- gsub(".*(rRNA)", "\\1", stats1$stitle)
     stats1$stitle <- gsub("(rRNA).*", "\\1", stats1$stitle)
+    stats1$abundance2 <- stats1$abundance
     stats1$abundance[stats1$stitle == "ribosomal_RNA"] <- NA
     stats1$abundance[stats1$stitle == "rRNA"] <- NA
-    stats1$abundance2 <- stats1$abundance
-    stats1 <- subset(stats1, select=c(3,1,5))
+    stats1 <- subset(stats1, select=c(3,4,5))
+    stats1$uniqseq <- 0
+    stats1$uniqseq[stats1$abundance >= 0] <- 1
     stats1$abundance <- ifelse(is.na(stats1$abundance), stats1$abundance2, stats1$abundance)
     stats1 <- subset(stats1, select=-c(abundance2))
+    stats2 <- ddply(stats1, c("phylum","uniqseq"), summarise, Nt = length(phylum))
+    stats3 <- as.data.frame(table(stats2$phylum)); colnames(stats3)[1] <- c("phylum")
+    stats1 <- merge(stats1, stats3, by=c("phylum"), all.x=TRUE)
+    stats1$abundance[stats1$uniqseq == "0" & stats1$Freq == "2" ] <- NA
     stats1 <- ddply(stats1, c("phylum"), summarise,
                     N    = length(phylum),
-                    mean = mean(abundance),
-                    sd   = sd(abundance),
-                    se   = sd / sqrt(N)
+                    mean = mean(abundance, na.rm=TRUE),
+                    sd   = sd(abundance, na.rm=TRUE)
     )
-    stats1 <- subset(stats1, select=c(1,3,2,5))
+    stats2 <- merge(stats2, stats3, by=c("phylum"), all.x=TRUE)
+    stats2$Nt[stats2$uniqseq == "0" & stats2$Freq == "2" ] <- NA; stats2 <- na.omit(stats2)
+    stats1 <- merge(stats1, stats2, by=c("phylum"), all.x=TRUE)
+    stats1$se <- stats1$sd/stats1$Nt    
+    stats1 <- subset(stats1, select=c(1,3,2,8))
     colnames(stats1) <- c("taxid","mean","uniq_reads","stderr")
     stats1$rel_stderr <- (stats1$stderr/stats1$mean)*100
   }
   if (nrow(stats1) == 0) { stats1 <- NULL; stats1 <- data.frame(matrix(nrow=0, ncol = 4)); colnames(stats1) <- c("taxid","mean","uniq_reads","stderr")}
   write.table(stats1,"stats1.txt", sep="\t",row.names=FALSE, col.names=FALSE, quote = F)
 }
+
