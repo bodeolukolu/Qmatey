c0dae467899c361500147d41b555b1c12d89a9e6
bodeolukolu <bolukolu@utk.edu>
2022-08-10 13:23:06 -0400

    abundance: diagnostic single-copy seq

--- scripts/Qmatey_profiling.sh
+++ scripts/Qmatey_profiling.sh
@@ -1916,15 +1916,16 @@ else
 	find ../alignment/ -size 0 -delete
 fi
 
+printf "MegaBLAST was completed" > ${projdir}/metagenome/MegaBLAST_completed.txt
 
 }
 cd $projdir
 metagout=$(ls ${projdir}/metagenome/haplotig/*metagenome.fasta.gz 2> /dev/null | wc -l)
 mblastout=$(ls ${projdir}/metagenome/alignment/cultured/ 2> /dev/null | grep 'megablast.gz' | grep -v 'combined_compressed.megablast.gz' | wc -l)
-if [[ "$metagout" -eq "$mblastout" ]] && [[ "$mblastout" -gt 0 ]]; then
-	echo -e "${YELLOW}- Qmatey has already performed ncbi megablast ${WHITE}"
+if test -f ${projdir}/metagenome/MegaBLAST_completed.txt; then
+	echo -e "${YELLOW}- Qmatey has already performed NCBI MegaBLAST ${WHITE}"
 else
-	echo -e "${YELLOW}- Qmatey is performing sequence alignment using ncbi megablast ${WHITE}"
+	echo -e "${YELLOW}- Qmatey is performing sequence alignment using NCBI MegaBLAST ${WHITE}"
 	time blast &>> ${projdir}/log.out
 fi
 
