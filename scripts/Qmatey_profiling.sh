50b9d5f035fe6599651c4c3d00c08bf7de0a1dbf
bodeolukolu <bolukolu@utk.edu>
2023-02-10 08:15:58 -0500

    automated minRD threshold determination

--- scripts/Qmatey_profiling.sh
+++ scripts/Qmatey_profiling.sh
@@ -1095,7 +1095,8 @@ ref_norm () {
 			for i in *.bam; do (
 				$samtools view -f 4 $i | awk '{print $1}' | \
 				awk -F '\t' 'NR==FNR{a[$0];next} $1 in a' - <(zcat ../samples/${i%.bam}_compressed.fasta.gz | awk '/^>/ {printf("%s%s\t",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' | awk '{gsub(/>/,"");}1') > ../samples/${i%.bam}_compressed.fasta &&
-				rm $i && gzip ../samples/${i%.bam}_compressed.fasta &&
+				rm $i ../samples/${i%.bam}_compressed.fasta.gz &&
+				gzip ../samples/${i%.bam}_compressed.fasta &&
 				wait ) &
 	      if [[ $(jobs -r -p | wc -l) -ge $gN ]]; then
 	      wait
