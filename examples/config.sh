861b9b84a77f7b692d5690d0dadcc1e25c151266
bodeolukolu <bolukolu@utk.edu>
2024-01-03 19:46:30 -0500

    update config.sh file

--- examples/config.sh
+++ examples/config.sh
@@ -27,7 +27,6 @@ normalization=true
 ####################################################
 blast_location=local
 local_db=/media/sdd/ncbi_db/nt/nt
-# local_db=/media/sdd/ncbi_db/nr/nr
 # local_db=/media/sdb/ncbi_db/16S/16S_ribosomal_RNA,/media/sdb/ncbi_db/18S/18S_fungal_sequences,/media/sdb/ncbi_db/28S/28S_fungal_sequences,/media/sdb/ncbi_db/ITS/ITS_eukaryote_sequences
 # local_db=/media/sdd/ncbi_db/refseq/refseq_rna,/media/sdd/ncbi_db/refseq/ref_viroids_rep_genomes,/media/sdd/ncbi_db/refseq/ref_prok_rep_genomes,/media/sdd/ncbi_db/refseq/ref_euk_rep_genomes
 taxids=true
